b468aae35c836aa2e610839531eecc7bde974d65
OverloadXyf <buct_yufengxiao@163.com>
2022-05-31 21:43:23 +0800

    add data

--- FlopsCounter/Counter.py
+++ FlopsCounter/Counter.py
@@ -2,15 +2,17 @@ import torch
 from ptflops import  get_model_complexity_info
 from models.SEnet import SELayer
 from models.CBAM import CBAM
-from models.ECAnet import ECALayer
+from models.ECAnet import ECALayer,ECALayer_Adaptive_Kernel
 
 with torch.cuda.device(0):
 
 
     # net=SELayer(256)
-    net=ECALayer()
+    # net=ECALayer()
+    # net=CBAM(256)
+    net=ECALayer_Adaptive_Kernel(512)
 
-    macs,params=get_model_complexity_info(net,(256,28,28),as_strings=True,print_per_layer_stat=True,verbose=True)
+    macs,params=get_model_complexity_info(net,(512,28,28),as_strings=True,print_per_layer_stat=True,verbose=True)
 
     print('{:<30}  {:<8}'.format('Computational complexity:',macs))
     print('{:<30}  {:<8}'.format('Number of parameters:',params))
